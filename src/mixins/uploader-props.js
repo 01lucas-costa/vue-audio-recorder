58713fda089cab720495b08b79fecac2ac8df46b
Lucas Costa <01lucascosta@gmail.com>
2019-03-11 10:04:14 -0300

    new prop

--- src/mixins/uploader-props.js
+++ src/mixins/uploader-props.js
@@ -1,7 +1,8 @@
 export default {
   props: {
-    filename  : { type: String, default: 'record'   },
-    headers   : { type: Object, default: () => ({}) },
-    uploadUrl : { type: String                      }
+    filename:         { type: String, default: 'record'   },
+    headers:          { type: Object, default: () => ({}) },
+    uploadUrl:        { type: String                      },
+    handlerDataAudio: { type: Boolean, default: false     }
   }
 }
